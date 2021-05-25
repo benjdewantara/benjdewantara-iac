9e86478f958d7b9a50a79c8ee7a7002d57a5873d
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-25 17:28:28 +0700

    Add resource IGW, RTB, Subnet, and Security Group

--- simple-http-server/main.tf
+++ simple-http-server/main.tf
@@ -19,7 +19,7 @@ variable "base_cidr_block" {
   default     = "122.110.81.0/24"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vpc" {
   # Referencing the base_cidr_block variable allows the network address
   # to be changed without modifying the configuration.
   cidr_block = var.base_cidr_block
@@ -29,35 +29,55 @@ resource "aws_vpc" "main" {
   }
 }
 
-output "aws_vpc_id" {
+output "main_vpc_id" {
   description = "The VPC ID"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.main_vpc.id
 }
 
 
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+output "main_igw_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.main_igw.id
+}
+
+
+resource "aws_route_table" "main_rtb" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    "Name" = "simpleHttpServerRtb"
+  }
+}
+
 resource "aws_subnet" "main_subnet" {
-  cidr_block = aws_vpc.main.cidr_block
-  vpc_id     = aws_vpc.main.id
+  cidr_block = aws_vpc.main_vpc.cidr_block
+  vpc_id     = aws_vpc.main_vpc.id
   tags = {
     "Name" = "simpleHttpServerSubnet"
   }
 }
 
-output "main_subnet_id" {
-
-}
-
 resource "aws_security_group" "main_secgroup" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main_vpc.id
   name   = "simpleHttpServerSg"
 }
 
 output "main_secgroup_id" {
   description = "The Security Group ID"
   value       = aws_security_group.main_secgroup.id
+
 }
 
-resource "aws_instance" "instance_simple_http_server" {
+resource "aws_instance" "main_ec2_instance" {
   ami                    = "ami-02f26adf094f51167"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.main_secgroup.id]
@@ -70,10 +90,10 @@ resource "aws_instance" "instance_simple_http_server" {
 
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.instance_simple_http_server.id
+  value       = aws_instance.main_ec2_instance.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.instance_simple_http_server.public_ip
+  value       = aws_instance.main_ec2_instance.public_ip
 }
