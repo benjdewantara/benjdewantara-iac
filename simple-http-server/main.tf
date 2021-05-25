ad2165638a8e54970ab18c6b606e7b89e3839394
Benyamin Manullang <benjdewantara@gmail.com>
2021-05-25 14:24:08 +0700

    VPC, Subnet, Security Group, and EC2 instance

--- simple-http-server/main.tf
+++ simple-http-server/main.tf
@@ -30,6 +30,50 @@ resource "aws_vpc" "main" {
 }
 
 output "aws_vpc_id" {
-    description = "The VPC ID"
-    value = aws_vpc.main.id
+  description = "The VPC ID"
+  value       = aws_vpc.main.id
+}
+
+
+resource "aws_subnet" "main_subnet" {
+  cidr_block = aws_vpc.main.cidr_block
+  vpc_id     = aws_vpc.main.id
+  tags = {
+    "Name" = "simpleHttpServerSubnet"
+  }
+}
+
+output "main_subnet_id" {
+
+}
+
+resource "aws_security_group" "main_secgroup" {
+  vpc_id = aws_vpc.main.id
+  name   = "simpleHttpServerSg"
+}
+
+output "main_secgroup_id" {
+  description = "The Security Group ID"
+  value       = aws_security_group.main_secgroup.id
+}
+
+resource "aws_instance" "instance_simple_http_server" {
+  ami                    = "ami-02f26adf094f51167"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.main_secgroup.id]
+  subnet_id              = aws_subnet.main_subnet.id
+
+  tags = {
+    Name = "SimpleHTTPServer"
+  }
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.instance_simple_http_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.instance_simple_http_server.public_ip
 }
