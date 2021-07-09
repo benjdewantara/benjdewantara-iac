ea173b6243346d6f192f01aca9a2242198cf6bd5
Benyamin Manullang <benjdewantara@gmail.com>
2021-07-09 20:14:57 +0700

    Create two S3 buckets named 'bucket-with-inventory' and 'bucket-with-inventory-output'. Then create two S3 Inventory of the first bucket, one producing Apache ORC format, another one producing Parquet

--- s3-bucket-and-inventory/main.tf
+++ s3-bucket-and-inventory/main.tf
@@ -14,3 +14,47 @@ provider "aws" {
   region  = "ap-southeast-1"
 }
 
+
+resource "aws_s3_bucket" "bucket_w_invtry" {
+  bucket = "bucket-with-inventory"
+}
+
+resource "aws_s3_bucket" "bucket_w_invtry_output" {
+  bucket = "bucket-with-inventory-output"
+}
+
+resource "aws_s3_bucket_inventory" "invtry_orc" {
+  bucket = aws_s3_bucket.bucket_w_invtry.id
+  name   = "EntireBucketDailyORC"
+
+  included_object_versions = "All"
+
+  schedule {
+    frequency = "Daily"
+  }
+
+  destination {
+    bucket {
+      format     = "ORC"
+      bucket_arn = aws_s3_bucket.bucket_w_invtry_output.arn
+    }
+  }
+}
+
+resource "aws_s3_bucket_inventory" "invtry_parquet" {
+  bucket = aws_s3_bucket.bucket_w_invtry.id
+  name   = "EntireBucketDailyParquet"
+
+  included_object_versions = "All"
+
+  schedule {
+    frequency = "Daily"
+  }
+
+  destination {
+    bucket {
+      format     = "Parquet"
+      bucket_arn = aws_s3_bucket.bucket_w_invtry_output.arn
+    }
+  }
+}
