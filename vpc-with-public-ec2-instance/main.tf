703923a55f17b6ca87295da321c377abdc8ae57f
Benyamin Manullang <benjdewantara@gmail.com>
2022-08-05 16:17:30 +0700

    Replace with benj

--- vpc-with-public-ec2-instance/main.tf
+++ vpc-with-public-ec2-instance/main.tf
@@ -3,117 +3,117 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_vpc" "vpc_horbo" {
+resource "aws_vpc" "vpc_benj" {
   cidr_block       = "122.110.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    "Name" = "vpc-horbo"
+    "Name" = "vpc-benj"
   }
 }
 
-resource "aws_subnet" "subnet_aza_horbo_apps" {
-  vpc_id            = aws_vpc.vpc_horbo.id
+resource "aws_subnet" "subnet_aza_benj_apps" {
+  vpc_id            = aws_vpc.vpc_benj.id
   cidr_block        = "122.110.0.0/19"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "subnet-aza-horbo-apps"
+    Name = "subnet-aza-benj-apps"
   }
 }
 
-resource "aws_subnet" "subnet_aza_horbo_web" {
-  vpc_id                  = aws_vpc.vpc_horbo.id
+resource "aws_subnet" "subnet_aza_benj_web" {
+  vpc_id                  = aws_vpc.vpc_benj.id
   cidr_block              = "122.110.32.0/19"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet-aza-horbo-web"
+    Name = "subnet-aza-benj-web"
   }
 }
 
-resource "aws_subnet" "subnet_azb_horbo_apps" {
-  vpc_id            = aws_vpc.vpc_horbo.id
+resource "aws_subnet" "subnet_azb_benj_apps" {
+  vpc_id            = aws_vpc.vpc_benj.id
   cidr_block        = "122.110.64.0/19"
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "subnet-azb-horbo-apps"
+    Name = "subnet-azb-benj-apps"
   }
 }
 
-resource "aws_subnet" "subnet_azb_horbo_web" {
-  vpc_id                  = aws_vpc.vpc_horbo.id
+resource "aws_subnet" "subnet_azb_benj_web" {
+  vpc_id                  = aws_vpc.vpc_benj.id
   cidr_block              = "122.110.96.0/19"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet-azb-horbo-web"
+    Name = "subnet-azb-benj-web"
   }
 }
 
-resource "aws_subnet" "subnet_azc_horbo_apps" {
-  vpc_id            = aws_vpc.vpc_horbo.id
+resource "aws_subnet" "subnet_azc_benj_apps" {
+  vpc_id            = aws_vpc.vpc_benj.id
   cidr_block        = "122.110.128.0/19"
   availability_zone = "us-east-1c"
 
   tags = {
-    Name = "subnet-azc-horbo-apps"
+    Name = "subnet-azc-benj-apps"
   }
 }
 
-resource "aws_subnet" "subnet_azc_horbo_web" {
-  vpc_id                  = aws_vpc.vpc_horbo.id
+resource "aws_subnet" "subnet_azc_benj_web" {
+  vpc_id                  = aws_vpc.vpc_benj.id
   cidr_block              = "122.110.160.0/19"
   availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet-azc-horbo-web"
+    Name = "subnet-azc-benj-web"
   }
 }
 
-resource "aws_internet_gateway" "igw_horbo" {
-  vpc_id = aws_vpc.vpc_horbo.id
+resource "aws_internet_gateway" "igw_benj" {
+  vpc_id = aws_vpc.vpc_benj.id
 
   tags = {
-    Name = "igw-horbo"
+    Name = "igw-benj"
   }
 }
 
-resource "aws_route_table" "rtb_horbo" {
-  vpc_id = aws_vpc.vpc_horbo.id
+resource "aws_route_table" "rtb_benj" {
+  vpc_id = aws_vpc.vpc_benj.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw_horbo.id
+    gateway_id = aws_internet_gateway.igw_benj.id
   }
 
   tags = {
-    Name = "rtb-horbo"
+    Name = "rtb-benj"
   }
 }
 
-resource "aws_route_table_association" "rtb_assoc_horbo_web_1" {
-  subnet_id      = aws_subnet.subnet_aza_horbo_web.id
-  route_table_id = aws_route_table.rtb_horbo.id
+resource "aws_route_table_association" "rtb_assoc_benj_web_1" {
+  subnet_id      = aws_subnet.subnet_aza_benj_web.id
+  route_table_id = aws_route_table.rtb_benj.id
 }
 
-resource "aws_route_table_association" "rtb_assoc_horbo_web_2" {
-  subnet_id      = aws_subnet.subnet_azb_horbo_web.id
-  route_table_id = aws_route_table.rtb_horbo.id
+resource "aws_route_table_association" "rtb_assoc_benj_web_2" {
+  subnet_id      = aws_subnet.subnet_azb_benj_web.id
+  route_table_id = aws_route_table.rtb_benj.id
 }
 
-resource "aws_route_table_association" "rtb_assoc_horbo_web_3" {
-  subnet_id      = aws_subnet.subnet_azc_horbo_web.id
-  route_table_id = aws_route_table.rtb_horbo.id
+resource "aws_route_table_association" "rtb_assoc_benj_web_3" {
+  subnet_id      = aws_subnet.subnet_azc_benj_web.id
+  route_table_id = aws_route_table.rtb_benj.id
 }
 
-# resource "aws_route_table_association" "rtb_assoc_horbo_apps_1" {
-# subnet_id      = aws_subnet.subnet_aza_horbo_apps.id
-# route_table_id = aws_route_table.rtb_horbo.id
+# resource "aws_route_table_association" "rtb_assoc_benj_apps_1" {
+# subnet_id      = aws_subnet.subnet_aza_benj_apps.id
+# route_table_id = aws_route_table.rtb_benj.id
 # }
 
 data "aws_ami" "amazon-linux-2" {
@@ -136,10 +136,10 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
-resource "aws_security_group" "secgroup_horbo_web" {
-  vpc_id      = aws_vpc.vpc_horbo.id
-  name        = "secgroup_horbo_web"
-  description = "This is security group secgroup_horbo_web"
+resource "aws_security_group" "secgroup_benj_web" {
+  vpc_id      = aws_vpc.vpc_benj.id
+  name        = "secgroup_benj_web"
+  description = "This is security group secgroup_benj_web"
 
   egress {
     cidr_blocks      = ["0.0.0.0/0"]
@@ -160,11 +160,11 @@ resource "aws_security_group" "secgroup_horbo_web" {
   }
 }
 
-resource "aws_iam_role" "iamrole_horbo_web" {
-  name = "iamrole_horbo_web"
+resource "aws_iam_role" "iamrole_benj_web" {
+  name = "iamrole_benj_web"
 
   inline_policy {
-    name = "iampolicy_inline_iamrole_horbo_web"
+    name = "iampolicy_inline_iamrole_benj_web"
     policy = jsonencode({
       "Version" : "2012-10-17",
       "Statement" : [
@@ -197,22 +197,22 @@ resource "aws_iam_role" "iamrole_horbo_web" {
 
 }
 
-resource "aws_iam_instance_profile" "iaminstanceprofile_horbo_web" {
-  name = "iaminstanceprofile_horbo_web"
-  role = aws_iam_role.iamrole_horbo_web.name
+resource "aws_iam_instance_profile" "iaminstanceprofile_benj_web" {
+  name = "iaminstanceprofile_benj_web"
+  role = aws_iam_role.iamrole_benj_web.name
 }
 
-resource "aws_instance" "ec2_instance_horbo_web" {
+resource "aws_instance" "ec2_instance_benj_web" {
   ami                         = data.aws_ami.amazon-linux-2.image_id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.subnet_aza_horbo_web.id
-  security_groups             = [aws_security_group.secgroup_horbo_web.id]
-  iam_instance_profile        = aws_iam_instance_profile.iaminstanceprofile_horbo_web.name
+  subnet_id                   = aws_subnet.subnet_aza_benj_web.id
+  security_groups             = [aws_security_group.secgroup_benj_web.id]
+  iam_instance_profile        = aws_iam_instance_profile.iaminstanceprofile_benj_web.name
 
 
   tags = {
-    Name = "ec2_instance_horbo_web"
+    Name = "ec2_instance_benj_web"
   }
 }
 
