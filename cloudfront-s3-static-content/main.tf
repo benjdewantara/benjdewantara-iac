72ab0ca22647d9bac22f525878905c2316b9e891
Benyamin Manullang <benyamin.feisal@xtremax.com>
2023-01-24 13:45:17 +0700

    Created CloudFront distribution for the S3 bucket origin. This still does not seem to work

--- cloudfront-s3-static-content/main.tf
+++ cloudfront-s3-static-content/main.tf
@@ -63,3 +63,44 @@ resource "aws_s3_object" "index_html" {
   </html>
   EOF
 }
+
+locals {
+  s3originid = "OriginId-${aws_s3_bucket.this.bucket}"
+}
+
+resource "aws_cloudfront_distribution" "this" {
+  origin {
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id   = local.s3originid
+  }
+
+  enabled = true
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3originid
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    "iacpath" = "cloudfront-s3-static-content/main.tf"
+  }
+}
