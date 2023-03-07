8dbc1510fdce4abc8b88c07c2cc86ca13ab86bd8
Benyamin Manullang <benjdewantara@gmail.com>
2023-03-08 00:48:36 +0700

    This still does not successfully do cp, rm, and echo "" > insertContent.txt

--- ec2-w-letsencrypt-certbot/user_data.sh
+++ ec2-w-letsencrypt-certbot/user_data.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -x
+sudo -s
+
+yum update -y
+
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/SSL-on-amazon-linux-2.html#letsencrypt
-sudo wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
-sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
-sudo yum-config-manager --enable epel*
-sudo yum repolist all
+wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+yum-config-manager --enable epel*
+yum repolist all
 
 <<instruction
 Edit the main Apache configuration file, /etc/httpd/conf/httpd.conf. Locate the "Listen 80" directive and add the following lines after it, replacing the example domain names with the actual Common Name and Subject Alternative Name (SAN).
@@ -16,9 +21,29 @@ Edit the main Apache configuration file, /etc/httpd/conf/httpd.conf. Locate the 
 </VirtualHost>
 instruction
 
-# sudo systemctl restart httpd
+sudo read -r -d '' insertThis << EOM
+Listen 80
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html"
+    ServerName "letsencrypt.example.id"
+    ServerAlias "www.letsencrypt.example.id"
+</VirtualHost>
+EOM
 
-sudo yum update -y
-sudo amazon-linux-extras install epel -y
-sudo yum install -y certbot python2-certbot-apache
-# sudo certbot
+sudo echo "" > /etc/httpd/conf/insertContent.txt
+sudo echo "$insertThis" > /etc/httpd/conf/insertContent.txt
+
+# unalias cp
+sudo /bin/cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
+
+sudo sed -e '/Listen 80/ {' -e "r /etc/httpd/conf/insertContent.txt" -e 'd' -e '}' -i /etc/httpd/conf/httpd.conf.bak
+
+# unalias rm
+sudo /bin/rm /etc/httpd/conf/insertContent.txt
+
+systemctl restart httpd
+
+yum update -y
+amazon-linux-extras install epel -y
+yum install -y certbot python2-certbot-apache
+# certbot
