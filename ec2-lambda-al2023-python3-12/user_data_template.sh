fba73375125e95e81c4ba7834249950d7983a245
Benyamin Dewantara <benjdewantara@gmail.com>
2025-07-23 12:01:42 +0700

    The line `virtualenv -p /usr/bin/$python3version python312custom` should hopefully use the newest `python3.12`. When invoking `pip install docker` after doing this, let's see if it uses `python3.12`, instead of `python3.9`

--- ec2-lambda-al2023-python3-12/user_data_template.sh
+++ ec2-lambda-al2023-python3-12/user_data_template.sh
@@ -4,10 +4,20 @@ echo "This is the start of ec2-lambda-al2023-python3-12/user_data_template.sh"
 sudo -s
 set -x
 
-cd
-
 HOME="/home/ec2-user"
+python3version="python3.12"
+
+cd || exit
+
 touch "$HOME/timestamp-before-isntalling-python-3-12"
 yum update -y
-# yum install -y python3.12
+
 yum install -y pip
+yum install -y $python3version
+
+pip install --user pipenv
+
+virtualenv -p /usr/bin/$python3version python312custom
+# shellcheck disable=SC3046
+source $HOME/python312custom/bin/activate
+pip install docker
