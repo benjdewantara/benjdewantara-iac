b142610cf4da85c599f32b2bafd0d9298f2d2a2d
Benyamin Dewantara <benjdewantara@gmail.com>
2025-07-26 00:35:55 +0700

    Gave up using the official way (i.e. https://nodejs.org/en/download). Will use `yum install` instead

--- ec2-nodejs-hello-world-server/user_data_template.sh
+++ ec2-nodejs-hello-world-server/user_data_template.sh
@@ -1,30 +1,38 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 echo "This is the start of ec2-nodejs-hello-world-server/user_data_template.sh"
 
-HOME="/home/ec2-user"
+#HOME="/home/ec2-user"
+#HOME="/root"
 
-set -x
+#set -x
 
-cd $HOME || exit
+#sudo su ec2-user
+#whoami
+#echo $HOME
+#cd $HOME || exit
 
 yum update -y
+yum install -y nodejs
 
-# Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
-
-# Download and install Node.js:
-nvm install 22
-
-# Verify the Node.js version:
-node -v # Should print "v22.17.1".
-nvm current # Should print "v22.17.1".
-
-# Verify npm version:
-npm -v # Should print "10.9.2".
-
-# Should automatically install latest npm
-npm install -g npm
+#export NVM_DIR="/home/ec2-user/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#
+## Download and install nvm:
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+#
+## in lieu of restarting the shell
+#\. "$HOME/.nvm/nvm.sh"
+#
+## Download and install Node.js:
+#nvm install 22
+#
+## Verify the Node.js version:
+#node -v # Should print "v22.17.1".
+#nvm current # Should print "v22.17.1".
+#
+## Verify npm version:
+#npm -v # Should print "10.9.2".
+#
+## Should automatically install latest npm
+#npm install -g npm
