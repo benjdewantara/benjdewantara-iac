2a65440a0d6c82069141a2da95359f413b51050f
Benyamin Dewantara <benjdewantara@gmail.com>
2026-01-05 15:43:29 +0700

    Wizard requires `amazon-cloudwatch-agent` installed

--- ec2-public-instance/user_data.sh
+++ ec2-public-instance/user_data.sh
@@ -4,5 +4,6 @@ echo "This is the start of ec2-public-instance\user_data.sh"
 
 set -x
 yum update -y
+yum install -y amazon-cloudwatch-agent
 
 echo "This is the end of ec2-public-instance\user_data.sh"
