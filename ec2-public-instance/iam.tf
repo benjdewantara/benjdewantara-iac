6e685bd88a1ccd93227ea74e68a127f77fc12284
Benyamin Dewantara <benjdewantara@gmail.com>
2026-01-05 16:10:04 +0700

    Matter of naming

--- ec2-public-instance/iam.tf
+++ ec2-public-instance/iam.tf
@@ -10,7 +10,7 @@ module "iam_policy" {
       "Version" : "2012-10-17",
       "Statement" : [
         {
-          "Sid" : "AllowCloudWatchAgentWizard",
+          "Sid" : "AllowCloudWatchAgentConfigurerWizard",
           "Effect" : "Allow",
           "Action" : [
             "cloudwatch:*",
