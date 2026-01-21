4a7b2eaa6d76887ac6c1b3de3c34335c1d3dbfae
Benyamin Dewantara <benjdewantara@gmail.com>
2026-01-21 19:56:27 +0700

    Escape backslash character, if any, in `$app_domain`

--- bnj-directus-tutor/user_data.sh
+++ bnj-directus-tutor/user_data.sh
@@ -211,9 +211,12 @@ adjust_personal_prefs() {
 adjust_personal_prefs
 
 replace_texts_in_plain() {
+  app_domain='http://${app_domain}:8055'
+  app_domain_escaped_backslash=${app_domain//\//\\/}
+
   while IFS= read -r -d '' file; do
     # shellcheck disable=SC2016
-    sed -i $file -E -e ' s/%APP_URI%/http://${app_domain}:8055/g '
+    sed -i $file -E -e " s/%APP_URI%/$app_domain_escaped_backslash/g "
   done < <(find mydir -mtime -7 -name '*.html' -print0)
 }
 replace_texts_in_plain
