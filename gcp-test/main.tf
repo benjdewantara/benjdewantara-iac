c94305ece7faf54a6c2d85d4ac74670b9264bb81
Benyamin Dewantara <benjdewantara@gmail.com>
2026-03-19 23:58:28 +0700

    Put project name as one of `locals`

--- gcp-test/main.tf
+++ gcp-test/main.tf
@@ -1,9 +1,13 @@
-provider "google" {
+locals {
   project = "heroic-passkey-317306"
 }
 
+provider "google" {
+  project = local.project
+}
+
 resource "google_service_account" "service_account" {
-  project      = "heroic-passkey-317306"
-  account_id   = "service-account-id"
+  project      = local.project
+  account_id   = "something-id"
   display_name = "Service Account"
 }
