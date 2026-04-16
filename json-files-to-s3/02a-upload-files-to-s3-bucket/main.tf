a9f793108ea1a43d622b093a428786901f6c14a2
Benyamin Dewantara <benjdewantara@gmail.com>
2026-04-16 15:37:21 +0700

    Try recursively list all files, given a relative path that reaches outside of `path.module`

--- json-files-to-s3/02a-upload-files-to-s3-bucket/main.tf
+++ json-files-to-s3/02a-upload-files-to-s3-bucket/main.tf
@@ -7,24 +7,25 @@ data "aws_s3_bucket" "this" {
   bucket = var.s3_bucket
 }
 
-resource "aws_s3_object" "this" {
-  for_each = fileset("${path.module}/files_template", "*json")
-
-  bucket       = data.aws_s3_bucket.this.bucket
-  content_type = "text/plain"
-  key          = "files_template/${each.key}"
-  source       = "${path.module}/files_template/${each.key}"
-  etag         = filemd5("${path.module}/files_template/${each.key}")
-}
-
 locals {
-  f = fileset("${path.module}/files_template", "*")
+  fileset_to_upload = fileset("${path.module}/../../sample_files_to_upload", "**")
 
   a26 = {
     hell = "worl"
   }
 }
 
+resource "aws_s3_object" "this" {
+  for_each = local.fileset_to_upload
+  # for_each = toset([])
+
+  bucket       = data.aws_s3_bucket.this.bucket
+  content_type = "text/plain"
+  key          = "${basename("${path.module}/../../sample_files_to_upload")}/${each.value}"
+  source       = "${"${path.module}/../../sample_files_to_upload"}/${each.value}"
+  etag         = filemd5("${"${path.module}/../../sample_files_to_upload"}/${each.value}")
+}
+
 output "filepaths" {
-  value = local.a26
+  value = local.fileset_to_upload
 }
