a0725eaaa514147949312181d2dfbb5abd83807f
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-12 16:57:42 +0700

    Copied everything from neighboring directory `bnj-directus-tutor` to the new `ec2-public-golang-gin`

---
+++ ec2-public-golang-gin/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  profile = "benj"
+  region  = "ap-southeast-1"
+}
+
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  projectname = "bnj-golang-gin-tutor"
+
+  azs          = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr_vpc     = "10.0.0.0/24"
+  cidrs_subnet = [for k, v in range(2 * length(local.azs)) : cidrsubnet(local.cidr_vpc, 3, k)]
+  create_vpc   = true
+
+  github_pat         = ""
+  uri_app_repository = "https://${local.github_pat}@github.com/benjdewantara/bnj-golang-gin-tutor.git"
+  s3_bucket_name     = local.projectname
+
+  zone_id    = ""
+  app_domain = "app-${random_string.this.result}.${data.aws_route53_zone.this.name}"
+}
+
+locals {
+  ec2_instance_name = "${local.projectname}-${random_string.this.result}"
+  time_now          = timestamp()
+}
+
+# this recreates resource random_string each time `terraform apply` occurs
+resource "random_string" "this" {
+  keepers = { marker = local.time_now }
+
+  length    = 4
+  lower     = true
+  min_lower = 4
+  special   = false
+}
+
+output "app_domain" {
+  value = aws_route53_record.this.name
+}
+
+output "app_domain_http" {
+  value = "http://${aws_route53_record.this.name}"
+}
---
+++ ec2-public-golang-gin/user_data.sh
@@ -0,0 +1,273 @@
+#!/usr/bin/env bash
+
+app_domain='${app_domain}'
+github_pat='${github_pat}'
+
+app_uri="http://$app_domain:8055"
+app_uri_backslash_escaped=$(echo $app_uri | sed -E -s ' s/\//\\\//g ')
+
+echo "This is the start of bnj-directus-tutor\user_data.sh"
+
+set -x
+yum update -y
+yum install -y amazon-cloudwatch-agent
+yum install -y docker
+yum install -y git
+yum install -y nc
+yum install -y jq
+# thanks to https://unix.stackexchange.com/a/249495/186480
+# use `yum list` to discover the exact `postgresql16.x86_64`
+yum install -y postgresql16.x86_64
+
+create_dummy_service_unit() {
+  local filename_shell_script="bnj-directus-frontend-plain-html.sh"
+  local filename_service_unit="bnj-directus-frontend-plain-html.service"
+
+  cat <<EOF >/home/ec2-user/$filename_shell_script
+#!/bin/bash
+while [[ 1 ]]; do
+  /home/ec2-user/app/plain/server.sh | nc -l 0.0.0.0 3000
+done;
+EOF
+
+  chmod +x /home/ec2-user/app/plain/server.sh
+  chmod +x /home/ec2-user/$filename_shell_script
+
+  cat <<EOF >/home/ec2-user/$filename_service_unit
+# Copyright Benyamin Manullang. All Rights Reserved.
+
+[Unit]
+Description=Benj test
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/home/ec2-user/$filename_shell_script
+KillMode=process
+Restart=always
+Environment="SERVER_DIR=/home/ec2-user/app/plain"
+WorkingDirectory=/home/ec2-user/app/plain
+RestartSec=15s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  systemctl enable /home/ec2-user/$filename_service_unit
+  systemctl start $filename_service_unit
+}
+create_dummy_service_unit
+
+logfrom_jctl() {
+  local service_unit_name_target=$1
+  service_unit_name_target=$${service_unit_name_target%.service}
+  local app_log_filepath="/var/log/app-$service_unit_name_target.log"
+
+  local logfrom_jctl_dir="/home/ec2-user/.logfrom_jctl"
+  local logfrom_jctl_service_unit_name="logfrom_jctl-$service_unit_name_target.service"
+  local logfrom_jctl_service_unit_file="$logfrom_jctl_dir/$logfrom_jctl_service_unit_name"
+  local logfrom_jctl_script="$logfrom_jctl_dir/logfrom_jctl-$service_unit_name_target.sh"
+
+  mkdir -p $logfrom_jctl_dir
+
+  cat <<EOF >$logfrom_jctl_script
+#!/bin/bash
+journalctl -u $service_unit_name_target --cursor-file '$logfrom_jctl_dir/$service_unit_name_target.cursor' >> $app_log_filepath
+EOF
+
+  chmod +x $logfrom_jctl_script
+
+  cat <<EOF >$logfrom_jctl_service_unit_file
+# Copyright Benyamin Manullang. All Rights Reserved.
+
+[Unit]
+Description=logfrom_jctl that creates log textfiles from given by journalctl
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=$logfrom_jctl_script
+Restart=always
+RestartSec=15
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  systemctl enable $logfrom_jctl_service_unit_file
+  systemctl start $logfrom_jctl_service_unit_name
+
+  chown -R ec2-user $logfrom_jctl_dir
+}
+logfrom_jctl "benj-svc-01a.service"
+
+install_node_npm_as_ec2user() {
+  cd /home/ec2-user || exit
+
+  # Download and install nvm:
+  set -x
+  export NVM_DIR="/home/ec2-user/.nvm"
+  mkdir -p $NVM_DIR
+  set +x
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+  # Download and install Node.js:
+  nvm install 24
+
+  # Verify the Node.js version:
+  node -v
+
+  # Verify npm version:
+  npm -v
+
+  npm install -g npm
+
+  chown -R ec2-user: /home/ec2-user/.nvm
+
+  dir_bin_node=$(find /home/ec2-user/.nvm/ -type d -iregex ".*versions.*bin" | head -n 1)
+  echo "export PATH=\$PATH:$dir_bin_node" >>/home/ec2-user/.bashrc
+}
+install_node_npm_as_ec2user
+
+install_pnpm() {
+  echo "Will install_pnpm"
+
+  curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+  local dir_pnpm='/home/ec2-user/.local/share/pnpm'
+  mkdir -p $dir_pnpm
+  cp -r '/root/.local/share/pnpm/'* '/root/.local/share/pnpm/.'* $dir_pnpm
+  chown -R ec2-user: '/home/ec2-user/.local'
+  echo "export PATH=\$PATH:$dir_pnpm" >>/home/ec2-user/.bashrc
+
+  echo "Finished install_pnpm"
+}
+install_pnpm
+
+install_followup_docker_compose() {
+  DOCKER_CONFIG="/usr/libexec/docker"
+  mkdir -p $DOCKER_CONFIG/cli-plugins
+  curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+}
+install_followup_docker_compose
+
+install_followup_docker() {
+  usermod -a -G docker ec2-user
+
+  # bash-completion for docker
+  curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+
+  systemctl start docker
+  systemctl enable docker.service
+  systemctl enable containerd.service
+}
+install_followup_docker
+
+clone_app_repository() {
+  git clone '${uri_app_repository}' /home/ec2-user/app
+  # cd /home/ec2-user && git clone '${uri_app_repository}'
+
+  while IFS= read -r -d '' file; do
+    # shellcheck disable=SC2016
+    echo "Will replace $file"
+    sed -i $file -E -e "s/%APP_URI%/$app_uri_backslash_escaped/g"
+  done < <(find '/home/ec2-user/app' -mtime -7 -name '*.html' -print0)
+
+  chown -R ec2-user: /home/ec2-user
+  find /home/ec2-user/app -type f -name '*.sh' -exec chmod +x {} \;
+}
+clone_app_repository
+
+replace_localhost_with_app_domain() {
+  dir_current=$(realpath .)
+  dir_directus=$(find '/home/ec2-user/app' -type d -name '*directus' | head -n 1)
+  dir_frontend=$(find '/home/ec2-user/app' -type d -name '*nextjs' | head -n 1)
+
+  echo "Will replace_localhost_with_app_domain"
+  [[ -z $app_domain ]] && echo "app_domain is not set, will not perform replacement" && return
+
+  local f="$dir_directus/.env"
+  sed -i "$f" -E -e " /PUBLIC_URL=/! b ; s/localhost/$app_domain/g "
+  sed -i "$f" -E -e " /REFRESH_TOKEN_COOKIE_DOMAIN=/! b ; s/localhost/$app_domain/g "
+  sed -i "$f" -E -e " /SESSION_COOKIE_DOMAIN=/! b ; s/localhost/$app_domain/g "
+  sed -i "$f" -E -e " /CONTENT_SECURITY_POLICY_DIRECTIVES__FRAME_SRC=/! b ; s/(http:\/\/)(localhost)(:[[:digit:]]+)/\0,\1$app_domain\3/g "
+
+  local f="$dir_frontend/.env"
+  sed -i "$f" -E -e " /NEXT_PUBLIC_DIRECTUS_URL=/! b ; s/localhost/$app_domain/g "
+  sed -i "$f" -E -e " /NEXT_PUBLIC_SITE_URL=/! b ; s/localhost/$app_domain/g "
+
+  echo "Will do 'docker compose up' on $dir_directus"
+  cd "$dir_directus" && docker compose up -d
+  # shellcheck disable=SC2164
+  cd "$dir_current"
+}
+replace_localhost_with_app_domain
+
+adjust_personal_prefs() {
+  local dir_home="/home/ec2-user"
+
+  if [[ -n $github_pat ]]; then
+    mkdir -p "$dir_home"
+    echo "export github_pat=$github_pat" >>$dir_home/.bashrc
+    chown -R ec2-user $dir_home/.bashrc
+  fi
+
+  echo 'set completion-ignore-case On' >>$dir_home/.inputrc
+  echo 'alias ll="ls -tral"' >>$dir_home/.bashrc
+
+  chown -R ec2-user: $dir_home/.inputrc
+  chown -R ec2-user: $dir_home/.bashrc
+}
+adjust_personal_prefs
+
+wait_until_port_8055_listens() {
+  while "true"; do
+    success=""
+    echo -e | nc localhost 8055 >/dev/null && success="yes"
+    [[ -z $success ]] || break
+    echo "Cannot detect a working localhost port 8055 yet. Will sleep for 5 seconds"
+    sleep 5
+  done
+
+  sleep 15
+}
+wait_until_port_8055_listens
+
+force_directus_admin_to_have_static_token() {
+  local response
+  local access_token
+  local headerAuth
+  local guidAdmin
+
+  response=$(
+    curl 'http://localhost:8055/auth/login' \
+      -X POST \
+      -H 'Content-Type: application/json' \
+      --data-raw '{"email":"admin@example.com","password":"admin1234"}'
+  )
+  access_token=$(echo $response | sed -E -e ' s/.+access_token":"([^"]+)".+/\1/ ')
+  [[ -z $access_token ]] && echo "Cannot determine static access_token of admin user guid" && exit 1
+
+  headerAuth="Authorization: Bearer $access_token"
+
+  response=$(
+    curl 'http://localhost:8055/users/me?fields=id' \
+      -X GET \
+      -H "$headerAuth"
+  )
+  guidAdmin=$(echo $response | jq -r .data.id)
+  [[ -z $guidAdmin ]] && echo "Cannot determine directus admin user guid" && exit 1
+
+  curl "http://localhost:8055/users/$guidAdmin" \
+    -X PATCH \
+    -H 'Content-Type: application/json' \
+    -H "$headerAuth" \
+    --data-raw '{"token":"UaTM1wd2Hpp7ho0SlU-azDaxoYjNcq0r"}'
+}
+force_directus_admin_to_have_static_token
+
+echo "This is the end of bnj-directus-tutor\user_data.sh"
