02dfaca91e13f54129504c1f49308f024c4d026d
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-12 16:48:48 +0700

    Copied everything from neighboring directory `bnj-directus-tutor` to the new `ec2-public-golang-gin`

---
+++ ec2-public-golang-gin/route53.tf
@@ -0,0 +1,11 @@
+data "aws_route53_zone" "this" {
+  zone_id = local.zone_id
+}
+
+resource "aws_route53_record" "this" {
+  name    = local.app_domain
+  type    = "CNAME"
+  zone_id = data.aws_route53_zone.this.zone_id
+  ttl     = 300
+  records = [module.ec2_this[0].public_dns]
+}
---
+++ ec2-public-golang-gin/sg.tf
@@ -0,0 +1,52 @@
+module "sg_this" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name            = local.projectname
+  description     = "Security group sample"
+  vpc_id          = module.vpc.vpc_id
+  use_name_prefix = false
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = -1
+      description = "Allow all incoming IPv4"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  ingress_with_ipv6_cidr_blocks = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = -1
+      description      = "Allow all incoming IPv6"
+      ipv6_cidr_blocks = "::/0"
+    },
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = -1
+      description = "Allow all outgoing IPv4"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  egress_with_ipv6_cidr_blocks = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = -1
+      description      = "Allow all outgoing IPv6"
+      ipv6_cidr_blocks = "::/0"
+    },
+  ]
+
+  tags = {
+    iacpath = "bnj-directus-tutor/sg.tf"
+  }
+}
