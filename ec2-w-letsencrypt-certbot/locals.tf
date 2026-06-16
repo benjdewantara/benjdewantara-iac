bfd3ed2c484be4288879e7b832b567b80be87989
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-16 18:04:14 +0700

    Put local variables into Terraform variables, assuming a local `terraform.tfvars.json` file exists

--- ec2-w-letsencrypt-certbot/locals.tf
+++ ec2-w-letsencrypt-certbot/locals.tf
@@ -1,13 +1,13 @@
 locals {
-  route53zoneid = "blank"
+  route53zoneid = var.route53zoneid
 
-  domain_name_public     = "blank.com"
+  domain_name_public     = var.domain_name_public
   domain_name_public_www = "www.${local.domain_name_public}"
 
-  email_certbot = "blank"
+  email_certbot = var.email_certbot
 
-  friendlyname = "blank"
+  friendlyname = var.friendlyname
 
-  s3_bucket_cert_upload = "s3://blank"
-  s3_bucket_region      = "xx-northeastsouthwest-99"
+  s3_bucket_cert_upload = var.s3_bucket_cert_upload
+  s3_bucket_region      = var.s3_bucket_region
 }
--- ec2-w-letsencrypt-certbot/variables.tf
+++ ec2-w-letsencrypt-certbot/variables.tf
@@ -1,5 +1,23 @@
-variable "ec2_keypair_name" {
-  description = "EC2 Key pair name"
+variable "domain_name_public" {
   type        = string
-  default     = "benj-keypair4"
+}
+
+variable "email_certbot" {
+  type        = string
+}
+
+variable "friendlyname" {
+  type        = string
+}
+
+variable "route53zoneid" {
+  type        = string
+}
+
+variable "s3_bucket_cert_upload" {
+  type        = string
+}
+
+variable "s3_bucket_region" {
+  type        = string
 }
