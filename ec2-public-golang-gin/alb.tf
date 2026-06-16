0c6a13d68cb35e0218bbb9296a55684d24a468e2
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-16 22:50:17 +0700

    This listens to incoming requests on port 11, then forwards it to EC2 instance on port 83. Just an example, will change later

--- ec2-public-golang-gin/alb.tf
+++ ec2-public-golang-gin/alb.tf
@@ -1,9 +1,36 @@
-resource "aws_alb" "this" {
+resource "aws_lb" "this" {
   name               = local.projectname
   subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.this.id]
   load_balancer_type = "application"
 
   tags = {
     iacpath = "ec2-public-golang-gin/alb.tf"
   }
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = local.projectname
+  port     = 83
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 11
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+
+  tags = {
+    iacpath = "ec2-public-golang-gin/alb.tf"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "this" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = module.ec2_this[0].id
 }
