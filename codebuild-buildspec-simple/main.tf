7570867489151075bee62e8ceeb92834d188bb03
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-18 13:04:38 +0700

    Successful simple CodeBuild creation with a buildspec file

---
+++ codebuild-buildspec-simple/main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  profile = "benj3"
+  region  = "ap-southeast-1"
+}
+
+locals {
+  nickname   = "bnj-test"
+  bucketname = "${local.nickname}-temporal"
+  time_now   = timestamp()
+}
+
+resource "random_string" "this" {
+  keepers = { marker = local.time_now }
+
+  length    = 4
+  lower     = true
+  min_lower = 4
+  special   = false
+}
+
+resource "aws_iam_role" "this" {
+  name = local.nickname
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "sts:AssumeRole"
+        ],
+        "Principal" : {
+          "Service" : [
+            "codebuild.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = local.bucketname
+}
+
+resource "aws_codebuild_project" "this" {
+  name         = local.nickname
+  service_role = aws_iam_role.this.arn
+
+  source {
+    type      = "NO_SOURCE"
+    buildspec = file("buildspec.yml")
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:7.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = false
+  }
+
+  artifacts {
+    location               = local.bucketname
+    name                   = "${local.nickname}-def"
+    namespace_type         = "NONE"
+    override_artifact_name = true
+    packaging              = "NONE"
+    path                   = "/"
+    type                   = "S3"
+  }
+}
