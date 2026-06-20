91d9cb2bed8e3612ac302f712b643d4a6f4adaa9
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-21 06:32:23 +0700

    Install `postgresql` then `postgresql-server`

--- ec2-pub-postgres/user_data.sh
+++ ec2-pub-postgres/user_data.sh
@@ -7,9 +7,19 @@ dir_user='/home/ec2-user' && mkdir -p $dir_user
 set -x
 yum update -y
 
+output_states_to_file() {
+  find /usr/ /etc/ -type f >"$dir_user/usr-etc-content.$timestamp_txt"
+  systemctl list-units >"$dir_user/systemctl-list-units.$timestamp_txt"
+}
+
 timestamp_txt=$(date --rfc-3339=s | sed -E -e 's/\W/./g')
-find /usr/ /etc/ -type f >"$dir_user/usr-etc-content.$timestamp_txt"
-systemctl list-units >"$dir_user/systemctl-list-units.$timestamp_txt"
+output_states_to_file
+
+yum install -y postgresql18.x86_64
+output_states_to_file
+
+yum install -y postgresql18-server.x86_64
+output_states_to_file
 
 chown -R ec2-user: $dir_user
 
