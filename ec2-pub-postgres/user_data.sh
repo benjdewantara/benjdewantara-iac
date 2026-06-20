3d18883ef2f52ee982ce637ce9c809e9220fe9ad
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-21 06:39:06 +0700

    Adjust the state output textfiles

--- ec2-pub-postgres/user_data.sh
+++ ec2-pub-postgres/user_data.sh
@@ -8,11 +8,12 @@ set -x
 yum update -y
 
 output_states_to_file() {
-  find /usr/ /etc/ -type f >"$dir_user/usr-etc-content.$timestamp_txt"
-  systemctl list-units >"$dir_user/systemctl-list-units.$timestamp_txt"
+  timestamp_txt=$(date --rfc-3339=s | sed -E -e 's/\W/./g')
+  find /usr/ /etc/ -type f >"$dir_user/$timestamp_txt.usr-etc-content"
+  systemctl list-units >"$dir_user/$timestamp_txt.systemctl-list-units"
 }
 
-timestamp_txt=$(date --rfc-3339=s | sed -E -e 's/\W/./g')
+# initial state
 output_states_to_file
 
 yum install -y postgresql18.x86_64
