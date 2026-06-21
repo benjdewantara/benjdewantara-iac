95f2061494ab48e7e47cf34e208a37e12d78f0ca
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-21 08:51:54 +0700

    Two shell functions modifying `pga_hba.conf` file, this changes the method of authenticating default user named `postgres`

--- ec2-pub-postgres/user_data.sh
+++ ec2-pub-postgres/user_data.sh
@@ -10,7 +10,7 @@ set -x
 yum update -y
 
 output_states_to_file() {
-  timestamp_txt=$(date --rfc-3339=s | sed -E -e 's/\W//g')
+  timestamp_txt=$(date --rfc-3339=s | sed -E -e ' s/\+.+//g ; s/\W//g')
   find /usr/ /etc/ -type f >"$dir_user/$timestamp_txt.usr-etc-content"
   systemctl list-units >"$dir_user/$timestamp_txt.systemctl-list-units"
 }
@@ -31,9 +31,25 @@ cp $pg_hba_filename $pg_hba_filename_bak
 systemctl enable postgresql.service
 systemctl start postgresql.service
 
-sudo -u postgres psql -c "ALTER USER postgres with password 'wkwk';"
-sed -i $pg_hba_filename -E -e ' s/(^local\s+.+\s)(\w+$)/\1md5/ '
-systemctl reload postgresql.service
+postgres_db_user_authenticate_with_password_wkwk() {
+  sudo -u postgres psql -c "ALTER USER postgres with password 'wkwk';"
+  sed -i $pg_hba_filename -E -e ' s/(^local\s+.+\s)(\w+$)/\1md5/ '
+  systemctl reload postgresql.service
+}
+#postgres_db_user_authenticate_with_password_wkwk
+
+# read https://www.postgresql.org/docs/current/auth-pg-hba-conf.html
+postgres_db_user_authenticate_unconditionally() {
+  sed -i $pg_hba_filename -E -e ' s/(^local\s+.+\s)(\w+$)/\1trust5/ '
+  sed -i $pg_hba_filename -E -e ' s/(^host\s+.+\s)(\w+$)/\1trust5/ '
+  systemctl reload postgresql.service
+}
+postgres_db_user_authenticate_unconditionally
+
+# documentations and articles that I read
+# https://www.postgresql.org/files/documentation/pdf/18/postgresql-18-US.pdf
+# https://stackoverflow.com/questions/18664074/getting-error-peer-authentication-failed-for-user-postgres-when-trying-to-ge
+# https://stackoverflow.com/questions/27107557/what-is-the-default-password-for-postgres
 
 # postgresql running
 output_states_to_file
@@ -41,10 +57,16 @@ output_states_to_file
 cd "$dir_user" || exit
 
 # download zitadel
-#set +x
-#LATEST=$(curl -i https://github.com/zitadel/zitadel/releases/latest | grep location: | cut -d '/' -f 8 | tr -d '\r'); ARCH=$(uname -m); case $ARCH in armv5*) ARCH="armv5";; armv6*) ARCH="armv6";; armv7*) ARCH="arm";; aarch64) ARCH="arm64";; x86) ARCH="386";; x86_64) ARCH="amd64";;  i686) ARCH="386";; i386) ARCH="386";; esac; wget -c https://github.com/zitadel/zitadel/releases/download/$LATEST/zitadel-linux-$ARCH.tar.gz -O - | tar -xz && sudo mv zitadel-linux-$ARCH/zitadel /usr/local/bin
-#set -x
-#output_states_to_file
+set +x
+LATEST=$(curl -i https://github.com/zitadel/zitadel/releases/latest | grep location: | cut -d '/' -f 8 | tr -d '\r')
+ARCH=$(uname -m)
+case $ARCH in armv5*) ARCH="armv5" ;; armv6*) ARCH="armv6" ;; armv7*) ARCH="arm" ;; aarch64) ARCH="arm64" ;; x86) ARCH="386" ;; x86_64) ARCH="amd64" ;; i686) ARCH="386" ;; i386) ARCH="386" ;; esac
+wget -c https://github.com/zitadel/zitadel/releases/download/$LATEST/zitadel-linux-$ARCH.tar.gz -O - | tar -xz && sudo mv zitadel-linux-$ARCH/zitadel /usr/local/bin
+set -x
+output_states_to_file
+
+# run zitadel (dont't do this on user data)
+#ZITADEL_DATABASE_POSTGRES_DSN=postgresql://root:postgres@localhost:5432/postgres?sslmode=disable ZITADEL_EXTERNALSECURE=false zitadel start-from-init --masterkey "MasterkeyNeedsToHave32Characters" --tlsMode disabled
 
 chown -R ec2-user: $dir_user
 
