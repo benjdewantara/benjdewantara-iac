d21d126f8b16ee3d2278a5e660bcb9bf17058e9b
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-21 07:17:55 +0700

    Download Zitadel after postgresql runs

--- ec2-pub-postgres/user_data.sh
+++ ec2-pub-postgres/user_data.sh
@@ -22,6 +22,19 @@ output_states_to_file
 yum install -y postgresql18-server.x86_64
 output_states_to_file
 
+postgresql-setup --initdb
+systemctl enable postgresql.service
+systemctl start postgresql.service
+
+# postgresql running
+output_states_to_file
+
+cd "$dir_user" || exit
+LATEST=$(curl -i https://github.com/zitadel/zitadel/releases/latest | grep location: | cut -d '/' -f 8 | tr -d '\r'); ARCH=$(uname -m); case $ARCH in armv5*) ARCH="armv5";; armv6*) ARCH="armv6";; armv7*) ARCH="arm";; aarch64) ARCH="arm64";; x86) ARCH="386";; x86_64) ARCH="amd64";;  i686) ARCH="386";; i386) ARCH="386";; esac; wget -c https://github.com/zitadel/zitadel/releases/download/$LATEST/zitadel-linux-$ARCH.tar.gz -O - | tar -xz && sudo mv zitadel-linux-$ARCH/zitadel /usr/local/bin
+
+# download zitadel
+output_states_to_file
+
 chown -R ec2-user: $dir_user
 
 echo "This is the end of ec2-pub-postgres\user_data.sh"
