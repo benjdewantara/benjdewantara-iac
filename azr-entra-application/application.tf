ff4b476df4027437b010dd61a969be4e387217bd
Benyamin Dewantara <benjdewantara@gmail.com>
2026-06-23 03:14:50 +0700

    Output client_secrets by renderin a template textfile, then output the textfile content

--- azr-entra-application/application.tf
+++ azr-entra-application/application.tf
@@ -1,5 +1,5 @@
 provider "azuread" {
-  tenant_id = "87f7da99-d436-4073-a47d-bbd48f2e8dd8"
+  tenant_id = var.tenant_id
 }
 
 data "azuread_client_config" "current" {}
@@ -10,10 +10,28 @@ resource "azuread_application_registration" "this" {
 
 resource "azuread_application_redirect_uris" "this" {
   application_id = azuread_application_registration.this.id
-  redirect_uris = ["http://localhost:8080"]
+  redirect_uris  = ["http://localhost:8080"]
   type           = "Web"
+}
+
+resource "azuread_application_password" "this" {
+  application_id = azuread_application_registration.this.id
+}
+
+data "template_file" "this" {
+  template = file("${path.module}/output.txt.template")
+
+  vars = {
+    client_secrets = azuread_application_password.this.value
+    client_id      = azuread_application_password.this.id
+  }
 }
 
 output "whoami" {
   value = data.azuread_client_config.current.id
+}
+
+output "client_secrets_id_file_content" {
+  value     = data.template_file.this.rendered
+  sensitive = true // comment this if you want to show the content to stdout
 }
