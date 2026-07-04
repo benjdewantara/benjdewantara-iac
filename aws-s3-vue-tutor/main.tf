f0a208addaad4d74c044087b3f0a28445013ed8c
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-05 00:04:44 +0700

    Turn on blocking public access

--- aws-s3-vue-tutor/main.tf
+++ aws-s3-vue-tutor/main.tf
@@ -14,3 +14,12 @@ resource "aws_s3_bucket_website_configuration" "this" {
     suffix = "index.html"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
