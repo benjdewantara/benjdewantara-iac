a418773e552f683e894ddad5d2b4d6428674665d
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-04 21:18:52 +0700

    Upload files to S3 after identifying a list of filenames within a local directory

---
+++ aws-s3-vue-tutor/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucketname
+}
+
+locals {
+  root_dir   = "/home/benjdewantara/Documents/bnj-vue-tutor/dist"
+  filenames  = fileset(local.root_dir, "**")
+
+  file_upload_specs = [for f in local.filenames : {
+    pathabs = join("/", [local.root_dir, f])
+    key     = f
+    }
+  ]
+}
+
+resource "aws_s3_object" "files" {
+  for_each = zipmap(
+    [for o in local.file_upload_specs : o.key],
+    [for o in local.file_upload_specs : o]
+  )
+
+  bucket = aws_s3_bucket.this.bucket
+  key    = each.value["key"]
+  source = each.value["pathabs"]
+  etag   = filemd5(each.value["pathabs"])
+}
---
+++ aws-s3-vue-tutor/variable.tf
@@ -0,0 +1,5 @@
+variable "aws_profile" { type = string }
+variable "aws_region" { type = string }
+variable "bucketname" { type = string }
+variable "project_name" { type = string }
+variable "vue_project_dir" { type = string }
