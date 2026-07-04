169cd20864be62f3036e0d0690aa87330e535475
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-05 00:01:52 +0700

    Put s3 object uploading in a separate .tf file

--- aws-s3-vue-tutor/main.tf
+++ aws-s3-vue-tutor/main.tf
@@ -14,26 +14,3 @@ resource "aws_s3_bucket_website_configuration" "this" {
     suffix = "index.html"
   }
 }
-
-locals {
-  root_dir  = var.root_dir
-  filenames = fileset(local.root_dir, "**")
-
-  file_upload_specs = [for f in local.filenames : {
-    pathabs = join("/", [local.root_dir, f])
-    key     = f
-    }
-  ]
-}
-
-resource "aws_s3_object" "files" {
-  for_each = zipmap(
-    [for o in local.file_upload_specs : o.key],
-    [for o in local.file_upload_specs : o]
-  )
-
-  bucket = aws_s3_bucket.this.bucket
-  key    = each.value["key"]
-  source = each.value["pathabs"]
-  etag   = filemd5(each.value["pathabs"])
-}
