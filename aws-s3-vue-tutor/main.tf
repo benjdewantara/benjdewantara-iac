7ad49ebf52f9d05431436c42c8cf4e0422c19625
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-05 00:13:20 +0700

    Turn off blocking public access. Create S3 bucket policy that allows public to read the content

--- aws-s3-vue-tutor/main.tf
+++ aws-s3-vue-tutor/main.tf
@@ -15,11 +15,33 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.bucket
+# resource "aws_s3_bucket_public_access_block" "this" {
+#   bucket = aws_s3_bucket.this.bucket
+#
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.bucket
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : [
+            "arn:aws:s3:::${aws_s3_bucket.this.bucket}/*"
+          ]
+        }
+      ]
+    }
+  )
 }
