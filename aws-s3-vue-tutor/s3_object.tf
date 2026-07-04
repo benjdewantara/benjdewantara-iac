a51beba603278500c1acf93b60fce7ef1e744559
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-05 01:39:52 +0700

    Specify content_type for html, css, js, and ico files. Otherwise let the file remain `application/octet-stream`

--- aws-s3-vue-tutor/s3_object.tf
+++ aws-s3-vue-tutor/s3_object.tf
@@ -1,10 +1,20 @@
 locals {
+  content_type_map = {
+    html = "text/html"
+    css  = "text/css"
+    js   = "text/javascript"
+    ico  = "image/vnd.microsoft.icon"
+  }
+}
+
+locals {
   root_dir  = var.root_dir
   filenames = fileset(local.root_dir, "**")
 
   file_upload_specs = [for f in local.filenames : {
-    pathabs = join("/", [local.root_dir, f])
-    key     = f
+    pathabs      = join("/", [local.root_dir, f])
+    content_type = lookup(local.content_type_map, element(split(".", f), -1), "application/octet-stream")
+    key          = f
     }
   ]
 }
@@ -15,9 +25,9 @@ resource "aws_s3_object" "files" {
     [for o in local.file_upload_specs : o]
   )
 
-  bucket = aws_s3_bucket.this.bucket
-  key    = each.value["key"]
-  source = each.value["pathabs"]
-  etag   = filemd5(each.value["pathabs"])
+  bucket       = aws_s3_bucket.this.bucket
+  key          = each.value["key"]
+  source       = each.value["pathabs"]
+  etag         = filemd5(each.value["pathabs"])
+  content_type = each.value["content_type"]
 }
-
