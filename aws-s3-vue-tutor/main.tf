fc1349044721fb28026ef0f7bc0e2a9c5b235385
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-04 21:55:53 +0700

    By creating `aws_s3_bucket_website_configuration` resource, the S3 static website hosting is enabled

--- aws-s3-vue-tutor/main.tf
+++ aws-s3-vue-tutor/main.tf
@@ -7,6 +7,14 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucketname
 }
 
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
 locals {
   root_dir  = var.root_dir
   filenames = fileset(local.root_dir, "**")
