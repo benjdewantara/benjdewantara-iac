504c21cdc969289a143fd2221ced9a246d6f83cf
Benyamin Dewantara <benjdewantara@gmail.com>
2026-07-20 14:43:20 +0700

    New experiment: `azr-entra-application`. Just to get myself to be aware of differences between an application and an application registration in Microsoft Entra

---
+++ azr-entra-application/application.tf
@@ -0,0 +1,37 @@
+provider "azuread" {
+  tenant_id = var.tenant_id
+}
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_application_registration" "this" {
+  display_name = var.app_display_name
+}
+
+resource "azuread_application_redirect_uris" "this" {
+  application_id = azuread_application_registration.this.id
+  redirect_uris  = ["http://localhost:8080/callback"]
+  type           = "Web"
+}
+
+resource "azuread_application_password" "this" {
+  application_id = azuread_application_registration.this.id
+}
+
+data "template_file" "this" {
+  template = file("${path.module}/output.txt.template")
+
+  vars = {
+    client_secrets = azuread_application_password.this.value
+    client_id      = azuread_application_password.this.id
+  }
+}
+
+output "whoami" {
+  value = data.azuread_client_config.current.id
+}
+
+output "client_secrets_id_file_content" {
+  value     = data.template_file.this.rendered
+  sensitive = true // comment this if you want to show the content to stdout
+}
