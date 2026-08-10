70c315c19df52ca27bd0b19814eed77e431ae031
Benyamin Dewantara <benjdewantara@gmail.com>
2026-08-10 11:45:45 +0700

    This assigns `user_existing` to `app_existing`

--- azr-entra-user-assignment/user.tf
+++ azr-entra-user-assignment/user.tf
@@ -18,34 +18,25 @@ output "a17" {
   value = data.azuread_application_published_app_ids.well_known
 }
 
-# resource "azuread_service_principal" "msgraph" {
-#   client_id    = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
-#   use_existing = true
-# }
-
-resource "azuread_service_principal" "user_existing" {
-  client_id = data.azuread_user.user_existing.object_id
+resource "azuread_service_principal" "app_existing" {
+  client_id    = data.azuread_application.app_existing.client_id
+  use_existing = true
 }
-
-# resource "azuread_service_principal" "app_existing" {
-#   client_id    = data.azuread_application.app_existing.client_id
-#   use_existing = true
-# }
 
 output "azuread_application_app_existing" {
   value = data.azuread_application.app_existing
 }
 
-# output "azuread_service_principal_app_existing" {
-#   value = azuread_service_principal.app_existing
-# }
+output "azuread_service_principal_app_existing" {
+  value = azuread_service_principal.app_existing
+}
 
 output "user_existing" {
   value = data.azuread_user.user_existing
 }
 
 resource "azuread_app_role_assignment" "this" {
-  app_role_id         = data.azuread_application.app_existing.app_roles[0].id
+  app_role_id         = azuread_service_principal.app_existing.app_roles[0].id
   principal_object_id = data.azuread_user.user_existing.object_id
-  resource_object_id  = data.azuread_application.app_existing.object_id
+  resource_object_id  = azuread_service_principal.app_existing.object_id
 }
