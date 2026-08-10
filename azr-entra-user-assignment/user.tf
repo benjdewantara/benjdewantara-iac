c979c5b22bf2d8adfabca68b093e82ff5de1b592
Benyamin Dewantara <benjdewantara@gmail.com>
2026-08-10 15:24:41 +0700

    This assigns the service principal of `user_existing to the service principal of``app_existing`

--- azr-entra-user-assignment/user.tf
+++ azr-entra-user-assignment/user.tf
@@ -8,27 +8,18 @@ data "azuread_user" "user_existing" {
   object_id = var.user_object_id
 }
 
-data "azuread_application" "app_existing" {
-  client_id = var.app_client_id
-}
-
 data "azuread_application_published_app_ids" "well_known" {}
 
 output "a17" {
   value = data.azuread_application_published_app_ids.well_known
 }
 
-resource "azuread_service_principal" "app_existing" {
-  client_id    = data.azuread_application.app_existing.client_id
-  use_existing = true
+data "azuread_service_principal" "app_existing" {
+  client_id = var.app_client_id
 }
 
 output "azuread_application_app_existing" {
-  value = data.azuread_application.app_existing
-}
-
-output "azuread_service_principal_app_existing" {
-  value = azuread_service_principal.app_existing
+  value = data.azuread_service_principal.app_existing
 }
 
 output "user_existing" {
@@ -36,7 +27,7 @@ output "user_existing" {
 }
 
 resource "azuread_app_role_assignment" "this" {
-  app_role_id         = azuread_service_principal.app_existing.app_roles[0].id
+  app_role_id         = data.azuread_service_principal.app_existing.app_roles[0].id
   principal_object_id = data.azuread_user.user_existing.object_id
-  resource_object_id  = azuread_service_principal.app_existing.object_id
+  resource_object_id  = data.azuread_service_principal.app_existing.object_id
 }
