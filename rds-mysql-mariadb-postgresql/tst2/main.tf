7f5222c54ce877a3ca47b45a2b2f39c8529717cf
Benyamin Manullang <benjdewantara@gmail.com>
2026-09-15 16:21:40 +0700

    Looks like `provider` block is required because I use `data "aws_availability_zones" "available"`

--- rds-mysql-mariadb-postgresql/tst2/main.tf
+++ rds-mysql-mariadb-postgresql/tst2/main.tf
@@ -1,20 +1,53 @@
 variable "aws_profile" { type = string }
 variable "projectname" { type = string }
 
+# module "vpc" {
+#   source = "../vpc"
+#
+#   aws_profile = var.aws_profile
+#   aws_region  = "ap-southeast-1"
+#   projectname = var.projectname
+#   vpc_cidr    = "10.0.0.0/24"
+# }
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  azs          = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr_vpc     = "10.0.0.0/24"
+  cidrs_subnet = [for k, v in range(2 * length(local.azs)) : cidrsubnet(local.cidr_vpc, 3, k)]
+}
+
+output "a21" {
+  value = local.cidrs_subnet
+}
+
 module "vpc" {
-  source = "../vpc"
+  source     = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git"
+  create_vpc = true
 
-  aws_profile = var.aws_profile
-  aws_region  = "ap-southeast-1"
-  projectname = var.projectname
-  vpc_cidr    = "10.0.0.0/24"
+  name = var.projectname
+  cidr = "10.0.0.0/24"
+
+  azs = local.azs
+  # intra_subnets  = [for i in range(0, 0 + 3) : local.cidrs_subnet[i]]
+  # private_subnets = [for i in range(0, 0 + 3) : local.cidrs_subnet[i]]
+  public_subnets = [for i in range(3, 3 + 3) : local.cidrs_subnet[i]]
+
+  create_igw = true
+
+  manage_default_security_group = false
+
+  tags = {
+    iacpath = "bnj-directus-tutor/vpc.tf"
+  }
 }
 
-module "rds" {
-  source = "../rds-single"
-
-  aws_profile = var.aws_profile
-  projectname = var.projectname
-  aws_region  = "ap-southeast-1"
-
-}
+# module "rds" {
+#   source = "../rds-single"
+#
+#   aws_profile = var.aws_profile
+#   projectname = var.projectname
+#   aws_region  = "ap-southeast-1"
+#   vpc_id      = module.vpc.vpc_id
+# }
--- rds-mysql-mariadb-postgresql/vpc/vpc.tf
+++ rds-mysql-mariadb-postgresql/vpc/vpc.tf
@@ -2,10 +2,24 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+data "aws_region" "current" {}
+
 resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
 
   tags = {
     Name = var.projectname
   }
 }
+
+# resource "aws_subnet" "this" {
+#   vpc_id = aws_vpc.this.id
+#
+#   availability_zone = "${data.aws_region.current.name}a"
+#   cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, 4, 1)
+# }
+
+# resource "aws_security_group" "this" {
+#
+# }
