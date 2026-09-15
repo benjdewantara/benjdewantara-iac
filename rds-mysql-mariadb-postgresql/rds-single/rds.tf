0dd1e9095568d66186fc7fe7d4670091ca2b54f2
Benyamin Manullang <benjdewantara@gmail.com>
2026-09-15 14:30:24 +0700

    Removed provider block because I still do not know what it does, so far it does not interfere with my simple scenarios

--- rds-mysql-mariadb-postgresql/rds-single/rds.tf
+++ rds-mysql-mariadb-postgresql/rds-single/rds.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.36.0"
-    }
-  }
-}
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
--- rds-mysql-mariadb-postgresql/tst2/main.tf
+++ rds-mysql-mariadb-postgresql/tst2/main.tf
@@ -2,7 +2,7 @@ variable "aws_profile" { type = string }
 variable "projectname" { type = string }
 
 module "rds" {
-  source = "../rds"
+  source = "../rds-single"
 
   aws_profile = var.aws_profile
   projectname = var.projectname
