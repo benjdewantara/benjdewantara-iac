147e74132f0a02ec201c11df9a52934eb6d8fff9
Benyamin Manullang <benjdewantara@gmail.com>
2026-09-15 10:47:50 +0700

    Use only main.tf. Get rid of variables.tf. Because tst is just for testing. Should be no need for much abstractions

--- rds-mysql-mariadb-postgresql/tst/main.tf
+++ rds-mysql-mariadb-postgresql/tst/main.tf
@@ -1,3 +1,8 @@
+variable "arn_iam_roles_incoming" { type = list(string) }
+variable "aws_profile" { type = string }
+variable "projectname" { type = string }
+# variable "specs" { type = object({}) }
+
 module "iam" {
   source = "../iam"
 
--- rds-mysql-mariadb-postgresql/tst/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "arn_iam_roles_incoming" { type = list(string) }
-variable "aws_profile" { type = string }
-variable "projectname" { type = string }
-# variable "specs" { type = object({}) }
