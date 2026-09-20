6e492accc1978bb09b0a1782ef2cd330e981150e
Benyamin Dewantara <benjdewantara@gmail.com>
2026-09-21 00:31:57 +0700

    In `that` account, IAM Role and CodeBuild is created. The IAM Role can be assumed by CodeBuild and the `this` AWS account

---
+++ aws-codebuild-that-send-to-this/main.tf
@@ -0,0 +1,23 @@
+variable "aws_profile_this" { type = string }
+variable "aws_profile_that" { type = string }
+
+provider "aws" {
+  alias   = "this"
+  profile = var.aws_profile_that
+  region  = "ap-southeast-1"
+}
+
+provider "aws" {
+  alias   = "that"
+  profile = var.aws_profile_that
+  region  = "ap-southeast-1"
+}
+
+module "that" {
+  source = "./that"
+
+  providers = {
+    aws = aws.that
+  }
+}
+
---
+++ aws-codebuild-that-send-to-this/that/main.tf
@@ -0,0 +1,56 @@
+data "aws_caller_identity" "that" {}
+
+locals {
+  arn_codebuild_format = "arn:aws:codebuild::%s:%s"
+  arn_codebuild        = format(local.arn_codebuild_format, data.aws_caller_identity.that.account_id, "*")
+}
+
+data "local_file" "that" {
+  filename = "./buildspec.yml"
+}
+
+resource "aws_iam_role" "that" {
+  name = "iamr-that"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "sts:AssumeRole"
+        ],
+        "Principal" : {
+          "AWS" : [
+            data.aws_caller_identity.that.account_id,
+          ],
+          "Service" : [
+            "codebuild.amazonaws.com"
+          ]
+        }
+      }
+    ]
+    }
+  )
+}
+
+resource "aws_codebuild_project" "that" {
+  name         = "cb-that"
+  service_role = aws_iam_role.that.arn
+
+  source {
+    type      = "NO_SOURCE"
+    buildspec = data.local_file.that.content
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:7.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = false
+  }
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+}
