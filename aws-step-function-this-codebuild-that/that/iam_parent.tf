14959a1a56dd286da0a91a490c2156105644c7f2
Benyamin Manullang <benjdewantara@gmail.com>
2026-09-21 14:00:30 +0700

    Actually `ProjectName` parameter is still required. Because the AWS CodeBuild StartBuild API Call requires that (see https://docs.aws.amazon.com/cli/latest/reference/codebuild/start-build.html). Since the Step Function execution is stuck despite CodeBuild having finished execution, add `codebuild:ListBuildsForProject`, and see if there may be behind-the-scene polling that is needed

--- aws-step-function-this-codebuild-that/that/iam_parent.tf
+++ aws-step-function-this-codebuild-that/that/iam_parent.tf
@@ -32,6 +32,7 @@ resource "aws_iam_role_policy" "that_codebuild_starter" {
           "Sid" : "CodeBuild",
           "Effect" : "Allow",
           "Action" : [
+            "codebuild:ListBuildsForProject",
             "codebuild:StartBuild",
           ],
           "Resource" : "*"
