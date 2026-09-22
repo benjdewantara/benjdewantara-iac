aa8139291173e47689d22adbc0da5d30979dc2c7
Benyamin Manullang <benjdewantara@gmail.com>
2026-09-22 11:07:49 +0700

    One-time S3 buckets for my experimentation purposes

---
+++ aws-s3-onetime-1a/main.tf
@@ -0,0 +1,17 @@
+variable "aws_profile_this" { type = string }
+variable "names_bucket" { type = list(string) }
+
+provider "aws" {
+  profile = var.aws_profile_this
+  region  = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "this" {
+  for_each = var.names_bucket
+
+  bucket = each.value
+
+  tags = {
+    iacpath = "aws-s3-onetime-1a/main.tf"
+  }
+}
