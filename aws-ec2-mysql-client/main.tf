e496f6133593bb55c57c17294c9056dce4b37d5c
Benyamin Manullang <benjdewantara@gmail.com>
2026-09-24 13:31:03 +0700

    A Route Table with route to Internet Gateway is created and associated with the public subnet. This still does not have security group. So the EC2 instance should still not be reachable

--- aws-ec2-mysql-client/main.tf
+++ aws-ec2-mysql-client/main.tf
@@ -58,6 +58,34 @@ resource "aws_subnet" "this_public" {
   }
 }
 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name    = "${var.projectname}-public"
+    iacpath = "aws-ec2-mysql-client/main.tf"
+  }
+}
+
+resource "aws_route_table" "this_public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name    = "${var.projectname}-public"
+    iacpath = "aws-ec2-mysql-client/main.tf"
+  }
+}
+
+resource "aws_route_table_association" "this_public" {
+  route_table_id = aws_route_table.this_public.id
+  subnet_id      = aws_subnet.this_public.id
+}
+
 data "aws_ami" "al2023" {
   most_recent = true
   owners      = ["amazon"]
